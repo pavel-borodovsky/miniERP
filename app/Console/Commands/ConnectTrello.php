cedd91e6a52c4ae5c20cb7dae85bcfe2a6896a45
Arston <pavel.borodovski@gmail.com>
2023-02-08 17:03:17 +0300

    sync data for bookers

--- app/Console/Commands/ConnectTrello.php
+++ app/Console/Commands/ConnectTrello.php
@@ -5,6 +5,7 @@ namespace App\Console\Commands;
 use App\Mail\SynchTrelloMessage;
 use App\Models\Board;
 use App\Models\BoardList;
+use App\Models\Booker;
 use App\Models\ListCard;
 use App\Models\Member;
 use App\Models\MemberCard;
@@ -34,7 +35,6 @@ class ConnectTrello extends Command
      */
     protected $description = 'Getting data via API Trello and updating model\'s data';
     //используется в кач-ве букера
-    protected $memberId = 'test_tale';
 
     /**
      * Execute the console command.
@@ -43,129 +43,131 @@ class ConnectTrello extends Command
      */
     public function handle()
     {
-        $createdBoards = $updatedBoards = $createdLists = $updatedLists = $createdCards = $updatedCards = $cardsWithMultipleTags = 0;
-        $message = 'При синхронизации с Trello были выделены карточки с несколькими тегами: ';
-        $api = new TrelloApi();
-        //todo реализовать получение данных для всех букеров
-        $boards = $api->getBoardsByMember($this->memberId);
-        foreach ($boards as $board) {
-            $existBoard = Board::find($board['id']);
-            $data = [
-                'idBoard' => $board['id'],
-                'name' => $board['name']
-            ];
-            if (!$existBoard) {
-                Board::create($data);
-                $createdBoards++;
-            } else {
-                $existBoard->update($data);
-                $updatedBoards++;
-            }
-
-            $lists = $api->getListsByBoard($board['id']);
-            foreach ($lists as $list) {
-                $existList = BoardList::find($list['id']);
+        $bookers = Booker::all();
+        foreach ($bookers as $booker) {
+            $api = new TrelloApi($booker->trello_token);
+            $boards = $api->getBoardsByMember($booker->user->member->id);
+            $createdBoards = $updatedBoards = $createdLists = $updatedLists = $createdCards = $updatedCards = $cardsWithMultipleTags = 0;
+            $message = 'При синхронизации с Trello были выделены карточки с несколькими тегами: ';
+            foreach ($boards as $board) {
+                $existBoard = Board::find($board['id']);
                 $data = [
-                    'idList' => $list['id'],
-                    'name' => $list['name'],
-                    'pos' => $list['pos'],
-                    'idBoard' => $list['idBoard']
+                    'idBoard' => $board['id'],
+                    'name' => $board['name']
                 ];
-                if (!$existList) {
-                    BoardList::create($data);
-                    $createdLists++;
+                if (!$existBoard) {
+                    Board::create($data);
+                    $createdBoards++;
                 } else {
-                    $existList->update($data);
-                    $updatedLists++;
+                    $existBoard->update($data);
+                    $updatedBoards++;
                 }
-            }
-            $cards = $api->getCardsByBoard($board['id']);
-            foreach ($cards as $card) {
-                //creating or adding card
-                $existCard = ListCard::find($card['id']);
-                $tag = null;
-                $countTags = Str::substrCount($card['name'], ' #');
-                if ($countTags > 0) {
-                    $tags = Str::substr($card['name'], mb_strpos($card['name'], '#'));
-                    if ($countTags == 1) {
-                        $tag = $tags;
+
+                $lists = $api->getListsByBoard($board['id']);
+                foreach ($lists as $list) {
+                    $existList = BoardList::find($list['id']);
+                    $data = [
+                        'idList' => $list['id'],
+                        'name' => $list['name'],
+                        'pos' => $list['pos'],
+                        'idBoard' => $list['idBoard']
+                    ];
+                    if (!$existList) {
+                        BoardList::create($data);
+                        $createdLists++;
                     } else {
-                        $tags = explode(' ', $tags);
-                        $tag = $tags[0];
-                        $message .= "<br/>$card[name]";
-                        $cardsWithMultipleTags++;
+                        $existList->update($data);
+                        $updatedLists++;
                     }
                 }
-                $data = [
-                    'idCard' => $card['id'],
-                    'name' => $card['name'],
-                    'pos' => $card['pos'],
-                    'due' => $card['due'],
-                    'idList' => $card['idList'],
-                    'urlSource' => $card['shortUrl'], //or $card['url']
-                    'invoice_task_tag' => $tag == '#tag' ? $tag : null
-                ];
-                if (!$existCard) {
-                    ListCard::create($data);
-                    $createdCards++;
-                } else {
-                    $existCard->update($data);
-                    $updatedCards++;
-                }
-
-                //check members for card
-                $tempMember = null;
-                $members = $api->getMembersOfCard($card['id']);
-                foreach ($members as $member) {
-                    $tempMember = Member::find($member['id']);
-                    if (!$tempMember) {
-                        //adding user with faker's help
-                        $faker = Container::getInstance()->make(Generator::class);
-                        $user = User::create([
-                            'name' => $member['username'],
-                            'email' => $faker->unique()->safeEmail(),
-                            'password' => $faker->password(),
-                            'isActive' => 0
-                        ]);
-
-                        $tempMember = Member::create([
-                            'id' => $member['id'],
-                            'user_name' => $member['username'],
-                            'user_id' => $user->id
-                        ]);
-                    }
-                    //check existing that relation
-                    if (!$tempMember->listCards->find($card['id'])) {
-                        $tempMember->listCards()->attach($card['id']);
-                        //storing old value member without relation, need to rewrite
-                        $tempMember = Member::find($member['id']);
-                    }
-                }
-
-                $comments = $api->getCommentsByCard($card['id']);
-
-                //no reason to check comments without members linked with card
-                if($tempMember != null) {
-                    foreach ($comments as $comment) {
-                        $tempMember->listCards->find($card['id']);
-                        $text = $comment['data']['text'];
-                        //adding estimate hour into pivot table
-                        if (Str::startsWith($text, 'plus! 0/')) {
-                            $time = $this->getTimeFromComment($comment['data']['text']);
-                            $estHour = $time[1];
-                            $tempMember->listCards()->updateExistingPivot($card['id'], ['est_hour' => $estHour]);
+                $cards = $api->getCardsByBoard($board['id']);
+                foreach ($cards as $card) {
+                    //creating or adding card
+                    $existCard = ListCard::find($card['id']);
+                    $tag = null;
+                    $countTags = Str::substrCount($card['name'], ' #');
+                    if ($countTags > 0) {
+                        $tags = Str::substr($card['name'], mb_strpos($card['name'], '#'));
+                        if ($countTags == 1) {
+                            $tag = $tags;
+                        } else {
+                            $tags = explode(' ', $tags);
+                            $tag = $tags[0];
+                            $message .= "<br/>$card[name]";
+                            $cardsWithMultipleTags++;
                         }
-                        if (Str::startsWith($text, 'plus! ')) {
-                            $time = $this->getTimeFromComment($text);
-                            $note = $this->getNoteFromComment($text);
-                            $memberCardId = MemberCard::where(['list_card_idCard' => $card['id'], 'member_id' => $tempMember->id])->first()->id;
-                            if ($time[0] > 0 && MemberCardTime::where(['members_cards_id' => $memberCardId, 'date' => Carbon::parse($comment['date'])->format('Y-m-d H:i:s'), 'spent_time' => (double)$time[0]])->first() == null) {
-                                MemberCardTime::create([
-                                    'spent_time' => (double)$time[0],
-                                    'date' => Carbon::parse($comment['date'])->format('Y-m-d H:i:s'),
-                                    'note' => $note ?? null,
-                                    'members_cards_id' => $tempMember->listCards->find($card['id']) ? $tempMember->listCards->find($card['id'])->pivot->id : ''
-                                ]);
+                    }
+                    $data = [
+                        'idCard' => $card['id'],
+                        'name' => $card['name'],
+                        'pos' => $card['pos'],
+                        'due' => $card['due'],
+                        'idList' => $card['idList'],
+                        'urlSource' => $card['shortUrl'], //or $card['url']
+                        'invoice_task_tag' => $tag == '#tag' ? $tag : null
+                    ];
+                    if (!$existCard) {
+                        ListCard::create($data);
+                        $createdCards++;
+                    } else {
+                        $existCard->update($data);
+                        $updatedCards++;
+                    }
+
+                    //check members for card
+                    $tempMember = null;
+                    $members = $api->getMembersOfCard($card['id']);
+                    foreach ($members as $member) {
+                        $tempMember = Member::find($member['id']);
+                        if (!$tempMember) {
+                            //adding user with faker's help
+                            $faker = Container::getInstance()->make(Generator::class);
+                            $user = User::create([
+                                'name' => $member['username'],
+                                'email' => $faker->unique()->safeEmail(),
+                                'password' => $faker->password(),
+                                'isActive' => 0
+                            ]);
+
+                            $tempMember = Member::create([
+                                'id' => $member['id'],
+                                'user_name' => $member['username'],
+                                'user_id' => $user->id
+                            ]);
+                        }
+                        //check existing that relation
+                        if (!$tempMember->listCards->find($card['id'])) {
+                            $tempMember->listCards()->attach($card['id']);
+                            //storing old value member without relation, need to rewrite
+                            $tempMember = Member::find($member['id']);
+                        }
+                    }
+
+                    $comments = $api->getCommentsByCard($card['id']);
+
+                    //no reason to check comments without members linked with card
+                    if ($tempMember != null) {
+                        foreach ($comments as $comment) {
+                            $tempMember->listCards->find($card['id']);
+                            $text = $comment['data']['text'];
+                            //adding estimate hour into pivot table
+                            if (Str::startsWith($text, 'plus! 0/')) {
+                                $time = $this->getTimeFromComment($comment['data']['text']);
+                                $estHour = $time[1];
+                                $tempMember->listCards()->updateExistingPivot($card['id'], ['est_hour' => $estHour]);
+                            }
+                            if (Str::startsWith($text, 'plus! ')) {
+                                $time = $this->getTimeFromComment($text);
+                                $note = $this->getNoteFromComment($text);
+                                $memberCardId = MemberCard::where(['list_card_idCard' => $card['id'], 'member_id' => $tempMember->id])->first()->id;
+                                if ($time[0] > 0 && MemberCardTime::where(['members_cards_id' => $memberCardId, 'date' => Carbon::parse($comment['date'])->format('Y-m-d H:i:s'), 'spent_time' => (double)$time[0]])->first() == null) {
+                                    MemberCardTime::create([
+                                        'spent_time' => (double)$time[0],
+                                        'date' => Carbon::parse($comment['date'])->format('Y-m-d H:i:s'),
+                                        'note' => $note ?? null,
+                                        'members_cards_id' => $tempMember->listCards->find($card['id']) ? $tempMember->listCards->find($card['id'])->pivot->id : ''
+                                    ]);
+                                }
                             }
                         }
                     }
--- app/Services/TrelloApi.php
+++ app/Services/TrelloApi.php
@@ -8,13 +8,12 @@ class TrelloApi
 {
     private $baseUrl = 'https://api.trello.com/1';
     private $apiKey = '0b5cfd9c65673ebefc6702a753ab9b8f';
-    private $token = 'ATTA037207c2cb1f78ad47fac31c3b379c7f6ae893fc3aaa685a44803a7680f32fbbE61C6A51';
+    private $token;
 
-    /*public function __construct($token)
+    public function __construct($token)
     {
-        //$response = Http::asForm()->get("https://trello.com/1/authorize?expiration=never&scope=read,write,account&response_type=token&name=Server%20Token&key=$this->apiKey")->json();
         $this->token = $token;
-    }*/
+    }
 
     public function getBoardsByMember($id){
         return Http::get($this->baseUrl . "/members/$id/boards?key=$this->apiKey&token=$this->token")->json();
